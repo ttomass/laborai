15953aaeac29d0bdff673d7eca8dee630317fbc4
tomas <tomasrupsys@gmail.com>
2015-09-24 00:10:08 +0300

    skaiciai nuo 5 iki 15 ir ju kubai

---
+++ P1/P1-1/Program.cs
@@ -0,0 +1,22 @@
+﻿//------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//------------------------------------------------------------
+// Ciklas for
+//------------------------------------------------------------
+namespace ciklas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skaičiai nuo 5 iki 15 ir jų kvadratai:");
+            for (int i = 5; i <= 15; i++)
+                Console.WriteLine(" {0,3:d} {1,5:d} {2,7:d}", i, i * i, i*i*i);
+        }
+    }
+}
+//------------------------------------------------------------
