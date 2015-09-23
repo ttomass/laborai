f96e45e78b9510906a83f84bc5f88ed428dd836c
tomas <tomasrupsys@gmail.com>
2015-09-24 00:49:11 +0300

    pabaigiau 1.2. Sąlygos sakinys duotą dalį.

---
+++ P1/P1-2/P1-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//------------------------------------------------------------
+// Sąlygos sakinys
+// Simbolių paskirstymas į eilutes
+//------------------------------------------------------------
+namespace salygos_sakinys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char simbolis;
+            int bendrasKiekis, kiekisEiluteje;
+            Console.Write("Įveskite kiek iš viso bus simbolių: ");
+            bendrasKiekis = int.Parse(Console.ReadLine());
+            Console.Write("Įveskite kiek simbolių bus eilutėje: ");
+            kiekisEiluteje = int.Parse(Console.ReadLine());
+            Console.Write("Įveskite spausdinamą simbolį: ");
+            simbolis = (char) Console.Read();
+            for (int i = 1; i <= bendrasKiekis; i++)
+                if (i % kiekisEiluteje == 0) Console.WriteLine(simbolis);
+                else Console.Write(simbolis);
+            Console.WriteLine("");
+        }
+    }
+}
+//------------------------------------------------------------
