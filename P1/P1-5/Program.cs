e388eb445cff4b520ca385138066ea672d4f3526
tomas <tomasrupsys@gmail.com>
2015-09-24 02:07:17 +0300

    savarankiskas darbas

---
+++ P1/P1-5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            string rezultatas;
+            char simbolis;
+            Console.Write("Įveskite pirmą skaičių: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Įveskite antrą skaičių: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Įveskite operacijos simbolį: ");
+            simbolis = (char) Console.Read();
+
+            if (simbolis == '+') rezultatas = (a + b).ToString();
+            else if (simbolis == '-') rezultatas = (a - b).ToString();
+            else if (simbolis == '*') rezultatas = (a*b).ToString();
+            else if (simbolis == '/') rezultatas = (a/b).ToString();
+            else rezultatas = "ERROR";
+
+            Console.WriteLine("{0} {1} {2} = {3}", a, simbolis, b, rezultatas);
+        }
+    }
+}
