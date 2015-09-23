6890b5bb3d759bc4302d034f5e0c925daaf5b119
tomas <tomasrupsys@gmail.com>
2015-09-24 01:43:23 +0300

    savarankiskas darbas

--- P1/P1-4/Program.cs
+++ P1/P1-4/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace P1_4
+//------------------------------------------------------------
+// Sudėtingas if sakinys
+//------------------------------------------------------------
+namespace sudetingas_if
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double fx;
+            double x;
+            Console.Write(" Įveskite x reikšmę: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Clear(); // Išvalo langą
+            Console.SetCursorPosition(5, 6); // Nustato pradinę žymeklio padėtį
+            if (x >= -2 && x <= 0)
+                fx = Math.Exp(-x);
+            else
+                if (x > 0 && x <= 2)
+                    fx = 2 * Math.Pow(x, 2) + 1;
+                else
+                    fx = Math.Pow(x , -2);
+            Console.WriteLine("Reikšmė x = {0,3:f2}, fx = {1,5:f3}", x, fx);
         }
     }
 }
+//------------------------------------------------------------
