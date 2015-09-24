dd1cc1346947238390659130d30f7939695741e0
tomas <tomasrupsys@gmail.com>
2015-09-24 22:17:15 +0300

    2.1 Viena klasė

---
+++ P2/P2-1/Plyta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1
+{
+    public class Plyta
+    {
+        private int ilgis, plotis, aukstis;
+
+        public Plyta(int ilgis, int plotis, int aukstis)
+        {
+            this.ilgis = ilgis;
+            this.plotis = plotis;
+            this.aukstis = aukstis;
+        }
+
+        public int GautiIlgi()
+        {
+            return ilgis;
+        }
+        public int GautiPloti()
+        {
+            return plotis;
+        }
+        public int GautiAuksti()
+        {
+            return aukstis;
+        }
+    }
+}
---
+++ P2/P2-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plyta plyta1 = new Plyta(250, 120, 88);
+            Plyta plyta2 = new Plyta(240, 115, 71);
+            Plyta plyta3 = new Plyta(240, 115, 61);
+            Siena siena = new Siena(12, 0.23, 3);
+            Console.WriteLine("Į sieną pirmo tipo plytų telpa: {0}", SkaiciuotiPlytomis(plyta1, siena));
+            Console.WriteLine("Į sieną antro tipo plytų telpa: {0}", SkaiciuotiPlytomis(plyta2, siena));
+            Console.WriteLine("Į sieną trečio tipo plytų telpa: {0}", SkaiciuotiPlytomis(plyta3, siena));
+        }
+
+        static int SkaiciuotiPlytomis(Plyta plyta, Siena siena)
+        {
+            int plytosTuris, sienaPlytomis, viso;
+            double sienosTuris;
+            sienosTuris = siena.GautiAukstiMilimetrais()*siena.GautiIlgiMilimetrais()*siena.GautiPlotiMilimetrais();
+            plytosTuris = plyta.GautiAuksti()*plyta.GautiIlgi()*plyta.GautiPloti();
+            sienaPlytomis = (int) (sienosTuris/plytosTuris);
+            viso = 4*sienaPlytomis;
+            return viso;
+        }
+    }
+}
